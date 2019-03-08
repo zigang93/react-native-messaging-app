3f4460bea77d4e7a5be8f13d0ae5c08749290658
zigang93 <tanzigang93@gmail.com>
2019-03-08 16:45:12 +0800

    done toolbar

--- components/Toolbar.js
+++ components/Toolbar.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react'
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import PropTypes from 'prop-types';
 
+const ToolbarButton= ({title,onPress}) => (
+  <TouchableOpacity onPress={onPress}>
+    <Text style={styles.button}>{title}</Text>
+  </TouchableOpacity>
+);
+
+ToolbarButton.propTypes={
+  title: PropTypes.string.isRequired, 
+  onPress: PropTypes.func.isRequired,
+};
 export default class Toolbar extends Component {
 
   static propTypes = {
@@ -19,10 +29,76 @@ export default class Toolbar extends Component {
     onPressLocation: () => {},
   };
 
+  state = {
+    text: '',
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.isFocused !== this.props.isFocused) {
+      if (nextProps.isFocused) {
+        this.input.focus();
+      } else {
+        this.input.blur();
+      }
+    } 
+  }
+
+  setInputRef = (ref) => {
+    this.input = ref;
+  };
+
+  handleChangeText = (text) => { 
+    this.setState({ text });
+  };
+
+  handleSubmitEditing = () => { 
+    const { onSubmit } = this.props; 
+    const { text } = this.state;
+    if (!text) return;
+    onSubmit(text);
+    this.setState({ 
+      text: '' 
+    }); 
+  };
+
+  handleFocus = () => {
+    const { onChangeFocus } = this.props;
+    onChangeFocus(true);
+  };
+
+  handleBlur = () => {
+    const { onChangeFocus } = this.props;
+    onChangeFocus(false);
+  };
+
   render() {
+
+    const { onPressCamera, onPressLocation } = this.props; 
+    // Grab this from state!
+    const { text } = this.state;
+
     return (
-      <View style={styles.toolbar}> 
-        {/* ... */}
+      <View style={styles.toolbar}>
+        {/* Use emojis for icons instead! */}
+        <ToolbarButton 
+          title={'C'} 
+          onPress={onPressCamera} 
+        /> 
+        <ToolbarButton title={'L'} onPress={onPressLocation} /> 
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input} 
+            underlineColorAndroid={'transparent'} 
+            placeholder={'Type something!'} 
+            blurOnSubmit={false}
+            value={text} 
+            onChangeText={this.handleChangeText} 
+            onSubmitEditing={this.handleSubmitEditing}
+            ref={this.setInputRef}
+            onFocus={this.handleFocus}
+            onBlur={this.handleBlur}
+          /> 
+        </View>
       </View>
     )
   }
@@ -36,6 +112,23 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingLeft: 16,
     backgroundColor: 'white',
+  },
+  button: {
+    top: -2,
+    marginRight: 12,
+    fontSize: 20,
+    color: 'grey',
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,0.04)', borderRadius: 16,
+    paddingVertical: 4, paddingHorizontal: 12, backgroundColor: 'rgba(0,0,0,0.02)',
+  }, 
+  input: {
+    flex: 1,
+    fontSize: 18,
   },
 })
 
