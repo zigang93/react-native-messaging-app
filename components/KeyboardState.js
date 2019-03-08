f446d0f322ddc22dfd0782c08f39e5d964a20db9
zigang93 <tanzigang93@gmail.com>
2019-03-08 17:59:28 +0800

    complete keyboardstate

---
+++ components/KeyboardState.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react'
+import { Keyboard, Platform } from 'react-native'
+import PropTypes from 'prop-types';
+
+const INITIAL_ANIMATION_DURATION = 250;
+
+export default class KeyboardState extends Component {
+
+  static propTypes = {
+    layout: PropTypes.shape({
+      x: PropTypes.number.isRequired,
+      y: PropTypes.number.isRequired,
+      width: PropTypes.number.isRequired,
+      height: PropTypes.number.isRequired,
+    }).isRequired,
+    children: PropTypes.func.isRequired,
+  };
+
+  constructor(props) { 
+    super(props);
+    const { layout: { height } } = props;
+    this.state = {
+      contentHeight: height,
+      keyboardHeight: 0,
+      keyboardVisible: false,
+      keyboardWillShow: false,
+      keyboardWillHide: false,
+      keyboardAnimationDuration: INITIAL_ANIMATION_DURATION,
+    }; 
+  }
+
+  componentWillMount() {
+    if (Platform.OS === 'ios') {
+      this.subscriptions = [
+        Keyboard.addListener('keyboardWillShow', this.keyboardWillShow),
+        Keyboard.addListener('keyboardWillHide', this.keyboardWillHide),
+        Keyboard.addListener('keyboardDidShow', this.keyboardDidShow),
+        Keyboard.addListener('keyboardDidHide', this.keyboardDidHide),
+      ];
+    } else {
+      this.subscriptions = [
+        Keyboard.addListener('keyboardDidHide', this.keyboardDidHide),
+        Keyboard.addListener('keyboardDidShow', this.keyboardDidShow),
+      ];
+    } 
+  }
+
+  componentWillUnmount() {
+    this.subscriptions.forEach(subscription => subscription.remove());
+  }
+
+  keyboardWillShow = (event) => { 
+    this.setState({ 
+      keyboardWillShow: true 
+    });
+    this.measure(event);
+    
+  };
+
+  keyboardDidShow = () => { 
+    this.setState({
+      keyboardWillShow: false,
+      keyboardVisible: true, 
+    });
+    this.measure(event);
+  
+  };
+  keyboardWillHide = (event) => {
+    this.setState({ 
+      keyboardWillHide: true 
+    });
+    this.measure(event);
+  };
+
+  keyboardDidHide = () => { 
+    this.setState({
+      keyboardWillHide: false,
+      keyboardVisible: false 
+    });
+    this.measure(event);
+  };
+
+  render() {
+
+    const { children, layout } = this.props; 
+
+    const {
+      contentHeight,
+      keyboardHeight,
+      keyboardVisible,
+      keyboardWillShow,
+      keyboardWillHide,
+      keyboardAnimationDuration,
+    } = this.state;
+
+    return children({ 
+      containerHeight: layout.height, 
+      contentHeight, 
+      keyboardHeight, 
+      keyboardVisible,
+      keyboardWillShow,
+      keyboardWillHide,
+      keyboardAnimationDuration,
+    })
+  }
+}
---
+++ components/MeasureLayout.js
@@ -0,0 +1,47 @@
+import {Constants} from 'expo';
+import React, { Component } from 'react';
+import { Platform,StyleSheet,View } from 'react-native';
+import PropTypes from 'prop-types';
+
+export default class MeasureLayout extends Component {
+
+  static propTypes = {
+    children: PropTypes.func.isRequired,
+  };
+
+  state = { 
+    layout: null,
+  };
+
+  handleLayout = event => {
+    const { nativeEvent: { layout } } = event;
+    this.setState({ layout: {
+        ...layout, 
+        y:
+          layout.y +
+            (Platform.OS === 'android' ? Constants.statusBarHeight : 0),
+      },
+    }); 
+  };
+
+
+  render() {
+
+    const { children } = this.props; 
+    const { layout } = this.state;
+
+    // Measure the available space with a placeholder view set to flex 1
+    if (!layout) {
+      return <View onLayout={this.handleLayout} style={styles.container} />;
+    }
+
+    return children(layout);
+  }
+}
+
+
+const styles = StyleSheet.create({ 
+  container: {
+    flex: 1, 
+  },
+});
