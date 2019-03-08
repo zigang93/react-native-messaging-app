f60aaf51d20f52399976b3ea2278adf24c31cbe6
zigang93 <tanzigang93@gmail.com>
2019-03-08 15:08:21 +0800

    setup statusbar error msg

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Status from './components/Status';
 export default class App extends React.Component {
+
+  renderMessageList() {
+    return (
+      <View style={styles.content}></View>
+    ); 
+  }
+
+  renderInputMethodEditor() { 
+    return (
+      <View style={styles.inputMethodEditor}></View>
+    );
+  }
+
+  renderToolbar() { 
+    return (
+      <View style={styles.toolbar}></View>
+    );
+  }
+
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container}> 
+        <Status />
+        {this.renderMessageList()} 
+        {this.renderToolbar()} 
+        {this.renderInputMethodEditor()}
       </View>
     );
   }
@@ -15,7 +38,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  inputMethodEditor: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  toolbar: {
+    borderTopWidth: 1,
+    borderTopColor: 'rgba(0,0,0,0.04)',
+    backgroundColor: 'white',
   },
 });
